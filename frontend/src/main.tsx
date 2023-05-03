73e019da68d3149f2ae51ba33c91b09844098cc4
M4rceleq <maciaszczyk.marcel@gmail.com>
2023-05-03 19:41:20 +0200

    add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginView } from './views/LoginView';
+import { HomeView } from './views/HomeView';
+import { SessionDetailsView } from './views/SessionDetailsView';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeView />,
+  },
+  {
+    path: "login",
+    element: <LoginView />,
+  },
+  {
+    path: "sessions/:sessionId",
+    element: <SessionDetailsView />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ frontend/src/views/SessionDetailsView.tsx
@@ -0,0 +1,3 @@
+export const SessionDetailsView = () => {
+    return <div>SessionDetailsView</div>;
+}
